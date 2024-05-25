55e6554a5b0d24b3d1cf8990fd5df13b98f97f3d
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-05-25 17:52:54 -0500

    activacion de la navgeacion

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -43,52 +43,23 @@
             <nav :class="{ 'flex fixed inset-0 bg-slate-800 pt-20': open, 'hidden bg-transparent pt-0': !open }"
                 class="flex-col flex-grow pb-4 md:pb-0 hidden lg:flex lg:justify-between lg:flex-row gap-10 lg:gap-0 lg:items-center">
                 <div class="flex flex-col lg:flex-row gap-5 lg:gap-10 w-11/12 lg:w-auto mx-auto lg:mx-0">
-                    <a class="py-2 text-text16 font-satoshiBold text-white focus:outline-none focus:shadow-outline cursor-pointer"
+                    <a class="py-2 text-text16 text-white focus:outline-none focus:shadow-outline cursor-pointer {{ request()->routeIs('index') ? 'font-satoshiBold' : 'font-satoshiRegular' }}"
                         href="{{ route('index') }}">Inicio</a>
-                    <a class="py-2 text-text16 font-satoshiRegular text-white focus:outline-none focus:shadow-outline cursor-pointer"
+                    <a class="py-2 text-text16 text-white focus:outline-none focus:shadow-outline cursor-pointer 
+                    {{ request()->routeIs('propiedades') ? 'font-satoshiBold' : 'font-satoshiRegular' }}"
                         href="{{ route('propiedades') }}">Propiedades</a>
 
-
-                    {{-- <div @click.away="open = false" class="relative" x-data="{ open: false }">
-                        <button @click="open = !open"
-                            class="flex flex-row items-center w-full py-2 text-text16 font-satoshiRegular text-left bg-transparent md:w-auto md:inline focus:outline-none focus:shadow-outline text-white">
-                            <span>Propiedades</span>
-                            <svg fill="currentColor" viewBox="0 0 20 20" :class="{ 'rotate-180': open, 'rotate-0': !open }"
-                                class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-                                <path fill-rule="evenodd"
-                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                        </button>
-                    <div x-show="open" x-transition:enter="transition ease-out duration-100"
-                        x-transition:enter-start="transform opacity-0 scale-95"
-                        x-transition:enter-end="transform opacity-100 scale-100"
-                        x-transition:leave="transition ease-in duration-75"
-                        x-transition:leave-start="transform opacity-100 scale-100"
-                        x-transition:leave-end="transform opacity-0 scale-95"
-                        class="absolute left-0 w-full mt-2 origin-top-right rounded-md shadow-lg lg:w-48">
-                        <div class="px-2 py-2 bg-white rounded-md shadow w-full">
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                href="{{route('propiedades')}}">Propiedades</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                href="{{route('index')}}">Link #2</a>
-                            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                                href="{{route('index')}}">Link #3</a>
-                        </div>
-                    </div>
-                </div> --}}
-
-                    <a class="py-2 text-text16 font-satoshiRegular text-white focus:outline-none focus:shadow-outline cursor-pointer"
+                    <a class="py-2 text-text16 text-white focus:outline-none focus:shadow-outline cursor-pointer {{ request()->routeIs('nosotros') ? 'font-satoshiBold' : 'font-satoshiRegular' }}"
                         href="{{ route('nosotros') }}">Nosotros</a>
 
                     {{-- <a class="py-2 text-text16 font-satoshiRegular text-white focus:outline-none focus:shadow-outline cursor-pointer"
                         href="{{ route('blog', 0) }}">Blog</a> --}}
                     @if (count($blogs) > 0)
-                    <a class="py-2 text-text16 font-satoshiRegular text-white focus:outline-none focus:shadow-outline cursor-pointer"
+                    <a class="py-2 text-text16 text-white focus:outline-none focus:shadow-outline cursor-pointer {{ request()->routeIs('blog') ? 'font-satoshiBold' : 'font-satoshiRegular' }}"
                     href="{{ route('blog', 0) }}">Blog</a>
                     @endif
 
-                    <a class="py-2 text-text16 font-satoshiRegular text-white focus:outline-none focus:shadow-outline cursor-pointer"
+                    <a class="py-2 text-text16 text-white focus:outline-none focus:shadow-outline cursor-pointer {{ request()->routeIs('contacto') ? 'font-satoshiBold' : 'font-satoshiRegular' }}"
                         href="{{ route('contacto') }}">Contacto</a>
                 </div>
 
