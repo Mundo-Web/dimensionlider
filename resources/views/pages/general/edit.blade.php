caa2688664505b3efe2fc1c2e92ade2a6414d7d9
Jordan Daniel 27 <jordanmoreano.ppy@gmail.com>
2024-05-27 10:29:39 -0500

    update en generales

--- resources/views/pages/general/edit.blade.php
+++ resources/views/pages/general/edit.blade.php
@@ -418,7 +418,7 @@
                                                         </div>
                                                     </div>
 
-                                                    <h2
+                                                   {{--  <h2
                                                         class="md:col-span-5 text-lg font-semibold text-slate-800 mt-2 dark:text-white">
                                                         Descripción de la empresa</h2>
 
@@ -465,7 +465,7 @@
                                                                 class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                                                 placeholder="Headertop...">
                                                         </div>
-                                                    </div>
+                                                    </div> --}}
 
                                                     <!-- <div class="md:col-span-2">
                                             <label for="city">City</label>
